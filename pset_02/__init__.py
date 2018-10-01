682ce501002b28e14ffb6c69715d9db5a28b9e73
Joanna Saikali <joannasaikali@Joannas-MacBook-Pro.local>
2018-09-30 22:06:58 -0400

    fixed filepaths

--- pset_02/__init__.py
+++ pset_02/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 from pkg_resources import get_distribution, DistributionNotFound
-from load_datasets import load_data, load_words, load_vectors
+from pset_02.load_datasets import load_data, load_words, load_vectors
 
 """Top-level package for pset 02."""
 
@@ -9,7 +9,7 @@ __email__ = 'joanna.saikali@gmail.com'
 try:
     __version__ = get_distribution(__name__).version
 except DistributionNotFound:
-    package is not installed
+    #package is not installed
     from setuptools_scm import get_version
     import os
     __version__ = get_version(
