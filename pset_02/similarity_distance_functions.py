ee86b9f541558f1e6a97738e678e0b3252bc5ffe
Joanna Saikali <joannasaikali@Joannas-MacBook-Pro.local>
2018-10-07 20:19:50 -0400

    allowing username input for better pset3 functionality

--- pset_02/similarity_distance_functions.py
+++ pset_02/similarity_distance_functions.py
@@ -33,19 +33,22 @@ def my_hash(github_username='jsaikali'):
 """
 Compute the cosine distance between my response and every response in the class.
 """
-def find_distance(vector_filename):
+def find_distance(vector_filename, github_username='jsaikali'):
 
     # Read in the vectors dataframe
     vectors_df = pandas.read_csv(vector_filename, index_col=0)
 
+    # Get my hash
+    hashed_username = my_hash(github_username)
+    
     # Extract my personal vector
-    my_vec = vectors_df[vectors_df.index == my_hash()].values[0]
+    my_vec = vectors_df[vectors_df.index == hashed_username].values[0]
 
     # Define a distance function
     def my_distance(vec):
         return 1 - cosine_similarity(vec, my_vec)
 
-    distances = vectors_df[vectors_df.index != my_hash()].apply(my_distance, axis=1)
+    distances = vectors_df[vectors_df.index != hashed_username].apply(my_distance, axis=1)
 
     # Dropping distance = 0, 1, Null
     # Corresponds to myself, prompt instructions, and blank entries
